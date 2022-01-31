618211189b2ca7573f7522fd8d0faf3074f2d77f
fabogit <77982342+fabogit@users.noreply.github.com>
2022-02-01 00:15:09 +0100

    day 30
    arrays

--- 11_js-basics/app.js
+++ 11_js-basics/app.js
@@ -1,6 +1,3 @@
-let greetingText = "Hello! I'm a js function inside a script element";
-let year = 2022;
-alert(greetingText);
-greetingText = "Overwriting greetingText value";
-alert(greetingText);
-alert(year);
+hobbies = ["code", "computers", "bikes"];
+alert(hobbies);
+alert(hobbies[0]);
