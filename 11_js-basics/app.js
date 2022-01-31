47c77d73ae1eda305ab48da5a181b320511afd38
fabogit <77982342+fabogit@users.noreply.github.com>
2022-02-01 00:08:13 +0100

    day 30
    outsorcing js code

---
+++ 11_js-basics/app.js
@@ -0,0 +1,6 @@
+let greetingText = "Hello! I'm a js function inside a script element";
+let year = 2022;
+alert(greetingText);
+greetingText = "Overwriting greetingText value";
+alert(greetingText);
+alert(year);
