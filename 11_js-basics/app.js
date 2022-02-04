1321a184cb60caf346468a7c40c2c3bdc782bf63
fabogit <77982342+fabogit@users.noreply.github.com>
2022-02-04 22:57:49 +0100

    day 31
    performing operations

--- 11_js-basics/app.js
+++ 11_js-basics/app.js
@@ -1,3 +1,11 @@
-jobObject = { title: "Developer", place: "New York", salary: 50000 };
-alert(jobObject);
-alert(jobObject.place);
+let age = 32;
+let userName = "Max";
+let hobbies = ["Sports", "Cooking", "Reading"];
+let job = {
+  title: "Developer",
+  place: "New York",
+  salary: 50000,
+};
+
+let adultYears = age - 18;
+alert(adultYears);
