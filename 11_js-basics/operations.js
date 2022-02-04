3ad1fe41b94b3ba3b772cb67f8940faf35f04e0e
fabogit <77982342+fabogit@users.noreply.github.com>
2022-02-04 23:56:58 +0100

    day 32
    operations shortcuts

--- 11_js-basics/operations.js
+++ 11_js-basics/operations.js
@@ -3,3 +3,13 @@ console.log(10 - 4);
 console.log(10 * 4);
 console.log(10 / 4);
 console.log(10 % 4); // Modulus: 10 / 4 => 2 * 4 => Remainder: 2
+
+let result = 100;
+result++; // result = result + 1
+result--; // result = result - 1
+
+result += 5; // result = result + 5
+result -= 5; // result = result - 5
+result /= 5; // result = result / 5
+result *= 5; // result = result * 5
+console.log(result);
