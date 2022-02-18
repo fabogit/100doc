9ea67befda457724b8d5304a79e06c1bc6ff0566
fabogit <77982342+fabogit@users.noreply.github.com>
2022-02-18 23:59:58 +0100

    day 35
    Listening To User Input Events

--- 12_js-DOM/app.js
+++ 12_js-DOM/app.js
@@ -1,3 +1,4 @@
+// listening to click
 let paragraphElement = document.querySelector("p");
 
 function changeParagraphText() {
@@ -6,3 +7,12 @@ function changeParagraphText() {
 }
 
 paragraphElement.addEventListener("click", changeParagraphText);
+
+// listening to input
+function retriveUserInput() {
+  let enteredText = inputElement.value;
+  console.log(enteredText);
+}
+
+let inputElement = document.querySelector("input");
+inputElement.addEventListener("input", retriveUserInput);
