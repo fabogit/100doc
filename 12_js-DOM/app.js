b5ca1feaac8401c112aa6c384557a32d75c807bc
fabogit <77982342+fabogit@users.noreply.github.com>
2022-02-18 22:37:49 +0100

    day 34
    document.body  #text/element

--- 12_js-DOM/app.js
+++ 12_js-DOM/app.js
@@ -1,3 +1,12 @@
 console.dir(document);
 
 document.body.children[1].children[0].href = "https://google.com";
+
+// #text
+console.dir(document.body.firstChild)
+// #text and html elements
+console.dir(document.body.childNodes)
+// html element
+console.dir(document.body.firstElementChild)
+// html element
+console.dir(document.body.children)
