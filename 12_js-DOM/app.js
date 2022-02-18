89e0d2f123d3b4f78344e15a7950a0d0690febac
fabogit <77982342+fabogit@users.noreply.github.com>
2022-02-18 22:44:21 +0100

    day 34
    searching DOM element

--- 12_js-DOM/app.js
+++ 12_js-DOM/app.js
@@ -1,12 +1,17 @@
 console.dir(document);
 
-document.body.children[1].children[0].href = "https://google.com";
+// DOM drill
+// document.body.children[1].children[0].href = "https://google.com";
 
 // #text
-console.dir(document.body.firstChild)
+console.dir(document.body.firstChild);
 // #text and html elements
-console.dir(document.body.childNodes)
+console.dir(document.body.childNodes);
 // html element
-console.dir(document.body.firstElementChild)
+console.dir(document.body.firstElementChild);
 // html element
-console.dir(document.body.children)
+console.dir(document.body.children);
+
+// DOM query
+let anchorElement = document.getElementById("external-link");
+anchorElement.href = "https://google.com";
