e225dbf4aa6838ce4e2a55c688d9d89ca6c9f9ee
fabogit <77982342+fabogit@users.noreply.github.com>
2022-02-22 23:31:55 +0100

    day 36
    implemented constants

--- 12_js-DOM/demo/demo.js
+++ 12_js-DOM/demo/demo.js
@@ -1,13 +1,15 @@
-let productNameInputElement = document.getElementById("product-name");
-let remainingCharsElement = document.getElementById("remaining-chars");
+const productNameInputElement = document.getElementById("product-name");
+const remainingCharsElement = document.getElementById("remaining-chars");
 
-let maxAllowedChars = productNameInputElement.maxLength;
+// console.dir(productNameInputElement)
+
+const maxAllowedChars = productNameInputElement.maxLength;
 
 function updateRemainingCharachters(event) {
-  let enteredText = event.target.value;
-  let enteredTextLength = enteredText.length;
+  const enteredText = event.target.value;
+  const enteredTextLength = enteredText.length;
 
-  let remainingCharacters = maxAllowedChars - enteredTextLength;
+  const remainingCharacters = maxAllowedChars - enteredTextLength;
 
   remainingCharsElement.textContent = remainingCharacters;
 }
