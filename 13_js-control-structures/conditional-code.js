5ff7a1d5ae01d731efe60dd871deeefae8ea9e92
fabogit <77982342+fabogit@users.noreply.github.com>
2022-02-24 20:48:20 +0100

    day 38
    truthy & falsy values

--- 13_js-control-structures/conditional-code.js
+++ 13_js-control-structures/conditional-code.js
@@ -3,3 +3,17 @@ const myName = "fab";
 if (myName === "fab") {
   console.log("Hello!");
 }
+
+let isLoggedIn = true;
+
+if (!isLoggedIn) {
+  console.log("User is NOT logged in");
+}
+
+const enteredUserName = "fab";
+
+// '' or 0 are falsy values (=false)
+
+if (enteredUserName) {
+  console.log("Input is valid");
+}
