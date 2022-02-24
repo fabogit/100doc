b18de4166db5561b9571a84ea47cc3c6f4880590
fabogit <77982342+fabogit@users.noreply.github.com>
2022-02-24 21:01:20 +0100

    day 38
    'for of' (array) loop

--- 13_js-control-structures/loops.js
+++ 13_js-control-structures/loops.js
@@ -1,3 +1,19 @@
+
+// for loop
 for (let i = 0; i < 10; i++) {
   console.log(i);
+}
+
+/* 
+also works ->
+for (let i = 0; i < someArray.length; i++) {
+  console.log(someArray[i]);
+}
+*/
+
+// for or loop
+const users = ['max', 'anna', ' joel']
+
+for (const user of users) {
+  console.log(user)
 }
