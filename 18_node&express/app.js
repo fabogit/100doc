07613b10c0d93eb60b3134455592d859d0ae99d2
fabogit <fabo.dev.email@gmail.com>
2022-03-10 03:04:02 +0100

    day 48
    parsing user Data with express (urlencoded & POST)

--- 18_node&express/app.js
+++ 18_node&express/app.js
@@ -1,13 +1,31 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
 // localhost:3000/current-time
 app.get("/current-time", function (req, res) {
   res.send(`<h1>Current time is ${new Date().toISOString()}</h1>`);
 });
 // localhost:3000
 app.get("/", function (req, res) {
-  res.send("<h1>Hello Worls!}</h1>");
+  const htmlForm = `
+                    <form action="/store-user" method="POST">
+                      <label>Your Name</label>
+                      <input type="text" name="username">
+                        <button>Submit</button>
+                      </input>
+                    </form>`;
+  res.send(htmlForm);
+});
+
+app.post("/store-user", function (req, res) {
+  //
+  const userName = req.body.username;
+  console.log(userName);
+  res.send(`<h1>User ${userName} registered`);
 });
 
 app.listen(3000);
