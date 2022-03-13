c9875f4f9a850aa3beb0ffbf7993bd72317c10ec
fabogit <fabo.dev.email@gmail.com>
2022-03-13 22:52:22 +0100

    day 53
    importing & exporting code in NodeJS (module.exports)

--- 20_advanced-server-side-code_dynamic-routes&errors/util/restaurant-data.js
+++ 20_advanced-server-side-code_dynamic-routes&errors/util/restaurant-data.js
@@ -1,11 +1,19 @@
-const filePath = path.join(__dirname, "data", "restaurants.json");
+const fs = require("fs");
+const path = require("path");
 
-function getStoredRestaurants() {
+const filePath = path.join(__dirname, "..", "data", "restaurants.json");
+
+function getStoredRestaurantsUtil() {
   const fileData = fs.readFileSync(filePath);
   const storedRestaurants = JSON.parse(fileData);
   return storedRestaurants;
 }
 
-function storeRestaurants(storableRestaurants) {
-  fs.writeFileSync(filePath, JSON.stringify(restaurantsJson));
+function storeRestaurantsUtil(storableRestaurants) {
+  fs.writeFileSync(filePath, JSON.stringify(storableRestaurants));
 }
+
+module.exports = {
+  getStoredRestaurants: getStoredRestaurantsUtil,
+  storeRestaurants: storeRestaurantsUtil,
+};
