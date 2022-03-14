cd5e85cd2636342aa2e91f223c454f0de5c99874
fabogit <fabo.dev.email@gmail.com>
2022-03-14 23:23:24 +0100

    day 56
    improving code with async / await

--- 21_js-advanced/async.js
+++ 21_js-advanced/async.js
@@ -1,6 +1,6 @@
 const fs = require("fs/promises");
 
-function readFile() {
+/* function readFile() {
   // // read file in the callback
   // fs.readFile("data.txt", function callback(err, data) {
   //   if (err) throw err;
@@ -20,6 +20,19 @@ function readFile() {
     .catch(function (error) {
       console.log(error);
     });
+  console.log("DONE");
+} */
+
+// async/await
+async function readFile() {
+  let fileData;
+  try {
+    fileData = await fs.readFile("data.txt");
+  } catch (error) {
+    console.log(error);
+  }
+  console.log("READING");
+  console.log(fileData.toString());
   console.log("DONE");
 }
 
