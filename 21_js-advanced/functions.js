14ffacef13d36dbaa9c9b404001af538bfbabc79
fabogit <fabo.dev.email@gmail.com>
2022-03-14 02:18:45 +0100

    day 54
    rest parameters & the spread operator

--- 21_js-advanced/functions.js
+++ 21_js-advanced/functions.js
@@ -1,6 +1,44 @@
+// assign default value to param
 function greetUser(firstString, secondString = "defaultString") {
   console.log(`${firstString} ${secondString}`);
 }
 
 greetUser();
 greetUser("Hello", "World!");
+
+/* simple loop using array parameter
+function sumUp(numbers) {
+  let sum;
+
+  for (const number of numbers) {
+    sum += number;
+  }
+  return sum;
+}
+
+console.log(sumUp( [1, 5, 10, 11, 20] ));
+
+*/
+// alternative using rest parameters, will work for 1 to n params
+function sumUp(...numbers) {
+  let sum = 0;
+
+  for (const number of numbers) {
+    sum += number;
+  }
+  return sum;
+}
+
+// simple array
+console.log(sumUp([1, 5, 10, 11, 20]));
+
+// rest parameters (merge params list into array)
+console.log(sumUp(1, 5, 10, 11, 20));
+
+// spread operator (spread array into single params)
+const inputNumbers = [1, 5, 10, 11, 20];
+console.log(sumUp(...inputNumbers));
+
+// spread
+const values = [-5, 3, 10];
+console.log(Math.max(...values));
