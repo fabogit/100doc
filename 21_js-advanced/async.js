949c3c511f3d13ea3b2bda64d4d63aa028ebbe49
fabogit <fabo.dev.email@gmail.com>
2022-03-14 21:50:44 +0100

    day 56
    promises

--- 21_js-advanced/async.js
+++ 21_js-advanced/async.js
@@ -1,13 +1,19 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 
 function readFile() {
-  // read file in the callback
-  fs.readFile("data.txt", function callback(err, data) {
-    if (err) throw err;
-    console.log(data.toString());
+  // // read file in the callback
+  // fs.readFile("data.txt", function callback(err, data) {
+  //   if (err) throw err;
+  //   console.log(data.toString());
+  //   console.log("File parsing DONE");
+  //   // start another async task
+  // });
+
+  // promises
+  fs.readFile("data.txt").then(function (fileData) {
+    console.log(fileData.toString());
     console.log("File parsing DONE");
   });
-
   console.log("DONE");
 }
 
