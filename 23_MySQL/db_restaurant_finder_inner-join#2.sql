d19b6da8fdde25d126597d8478e6837af0656310
fabogit <fabo.dev.email@gmail.com>
2022-03-15 15:53:35 +0100

    day 59
    practice inner join

---
+++ 23_MySQL/db_restaurant_finder_inner-join#2.sql
@@ -0,0 +1,15 @@
+SELECT
+	reviews.*,
+	restaurants.name AS restaurant_name,
+	addresses.*,
+	types.name AS restaurant_type
+FROM
+	restaurant_finder.reviews
+INNER JOIN restaurants ON
+	reviews.restaurant_id = restaurants.id
+INNER JOIN addresses ON
+	restaurants.address_id = addresses.id
+INNER JOIN types ON
+	restaurants.type_id = types.id
+WHERE
+	rating > 3
