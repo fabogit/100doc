c1dcae057c3e1afa96d8686f4547c8a1c82c2bb1
fabogit <fabo.dev.email@gmail.com>
2022-03-15 14:39:37 +0100

    day 58
    addresses table

---
+++ 23_MySQL/table_addresses.sql
@@ -0,0 +1,9 @@
+CREATE TABLE if not exists `restaurant_finder`.`addresses` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`street` VARCHAR(255) NOT NULL,
+	`street_number` VARCHAR (45) NOT NULL,
+	`city` VARCHAR (255) NOT NULL,
+	`postal_code` INT NOT NULL,
+	`country` VARCHAR(255) NOT NULL,
+	PRIMARY KEY(`id`))
+ENGINE = InnoDB;
