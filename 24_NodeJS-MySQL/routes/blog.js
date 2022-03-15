02a5015f7ffb4ca3d0b95724f5dcf95745be955d
fabogit <fabo.dev.email@gmail.com>
2022-03-16 00:01:42 +0100

    day 62
    updating posts

--- 24_NodeJS-MySQL/routes/blog.js
+++ 24_NodeJS-MySQL/routes/blog.js
@@ -73,4 +73,16 @@ router.get("/posts/:id/edit", async function (req, res) {
   res.render("update-post", { post: posts[0] });
 });
 
+router.post("/posts/:id/edit", async function (req, res) {
+  const updateQuery = `UPDATE posts SET title = ?, summary = ?, body = ? WHERE id = ?`;
+
+  await db.query(updateQuery, [
+    req.body.title,
+    req.body.summary,
+    req.body.content,
+    req.params.id,
+  ]);
+  res.redirect("/posts");
+});
+
 module.exports = router;
