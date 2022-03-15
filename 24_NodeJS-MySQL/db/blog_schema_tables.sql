e126cb0134ca9ac38c1f4284512eafbfe49c58e9
fabogit <fabo.dev.email@gmail.com>
2022-03-15 16:56:31 +0100

    day 60
    database initialization

---
+++ 24_NodeJS-MySQL/db/blog_schema_tables.sql
@@ -0,0 +1,20 @@
+DROP SCHEMA IF EXISTS `blog`;
+
+DROP TABLE IF EXISTS blog.authors, blog.post;
+
+CREATE SCHEMA `blog`;
+
+CREATE TABLE `blog`.`authors` (
+`id` INT NOT NULL AUTO_INCREMENT,
+`name` VARCHAR(255) NOT NULL,
+`email` VARCHAR(255) NOT NULL,
+PRIMARY KEY (`id`));
+
+CREATE TABLE `blog`.`post` (
+`id` INT NOT NULL AUTO_INCREMENT,
+`title` VARCHAR(255) NOT NULL,
+`summary` VARCHAR(255) NOT NULL,
+`body` TEXT NOT NULL,
+`date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+`author_id` INT NOT NULL,
+PRIMARY KEY (`id`));
