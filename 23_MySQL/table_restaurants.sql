22530bf9e68049c8a6d0590abed3ab2ef076ae1c
fabogit <fabo.dev.email@gmail.com>
2022-03-15 02:56:02 +0100

    day 58
    create table

---
+++ 23_MySQL/table_restaurants.sql
@@ -0,0 +1,7 @@
+-- create table
+CREATE TABLE `restaurant_finder`.`restaurants` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(255) NOT NULL,
+	`type` VARCHAR (255) NOT NULL,
+	PRIMARY KEY(`id`))
+ENGINE = InnoDB;
