4872717e1ba3f8469a82bd4919fdc779b101f25e
fabogit <fabo.dev.email@gmail.com>
2022-03-15 16:56:31 +0100

    day 60
    Database Initialization

---
+++ 24_NodeJS-MySQL/db/table_authors_data.sql
@@ -0,0 +1,12 @@
+INSERT
+	INTO
+	blog.authors (name,
+	email)
+VALUES ("Mario Rossi",'mariorossi@email.com'),
+("Francesco Bianchi",'francescobianchi@email.com'),
+('Giorgio Franchi','giorgiofranchi@email.com');
+
+SELECT
+	*
+FROM
+	blog.authors;
