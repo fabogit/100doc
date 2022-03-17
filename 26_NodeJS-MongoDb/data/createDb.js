b68757dec3eac698f11debcd1c18df8918499fe3
fabogit <fabo.dev.email@gmail.com>
2022-03-17 02:08:41 +0100

    day 65
    inserting documents (New Posts)

--- 26_NodeJS-MongoDb/data/createDb.js
+++ 26_NodeJS-MongoDb/data/createDb.js
@@ -5,7 +5,7 @@
 db.authors.insertMany(
   [
     {name: "Dante Alighieri", email: "alighieridante@email.com"},
-    {name: "Giovanni Verga", email: "vergagiovanni @email.com"},
+    {name: "Giovanni Verga", email: "vergagiovanni@email.com"},
     {name: "Eugenio Montale", email: "montaleeugenio@email.com"},
     {name: "Giacomo Leopardi", email: "leopardigiacomo@email.com"},
     {name: "Italo Calvino", email: "calvinoitalo@email.com"},
--- 26_NodeJS-MongoDb/routes/blog.js
+++ 26_NodeJS-MongoDb/routes/blog.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const mongodb = require("mongodb");
 
 const db = require("../data/database");
+const ObjectId = mongodb.ObjectId;
 
 const router = express.Router();
 
@@ -12,9 +14,32 @@ router.get("/posts", function (req, res) {
   res.render("posts-list");
 });
 
+router.post("/posts", async function (req, res) {
+  const authorId = new ObjectId(req.body.author);
+  const authorDocument = await db
+    .getDb()
+    .collection("authors")
+    .findOne({ _id: authorId });
+
+  const newPost = {
+    title: req.body.title,
+    summary: req.body.summary,
+    body: req.body.content,
+    date: new Date(),
+    author: {
+      id: authorId,
+      name: authorDocument.name,
+      email: authorDocument.email,
+    },
+  };
+
+  const result = await db.getDb().collection("posts").insertOne(newPost);
+  res.redirect("/posts");
+});
+
 router.get("/new-post", async function (req, res) {
   const authorsArray = await db.getDb().collection("authors").find().toArray();
-  res.render("create-post", { authors: authorsArray});
+  res.render("create-post", { authors: authorsArray });
 });
 
 module.exports = router;
