b724f61d1bd46e1f75b9518e2bcc05a1e2680746
fabogit <fabo.dev.email@gmail.com>
2022-04-10 23:32:40 +0200

    day 76
    adding fetch & fetchAll to the model

--- 31_good-code-refactoring-mvc/models/post.js
+++ 31_good-code-refactoring-mvc/models/post.js
@@ -1,5 +1,7 @@
 const db = require("../data/database");
 const mongodb = require("mongodb");
+const { restart } = require("nodemon");
+const { post } = require("../routes/blog");
 const ObjectId = mongodb.ObjectId;
 // const { ObjectId } = require("mongodb");
 
@@ -11,6 +13,22 @@ class Post {
     if (id) {
       this.id = new ObjectId(id);
     }
+  }
+
+  static async fetchAll() {
+    const posts = await db.getDb().collection("posts").find().toArray();
+    return post;
+  }
+
+  async fetch() {
+    if (!this.id) {
+      // throw new error || return
+      return;
+    }
+    
+    const postDocument = await db.getDb().collection("posts").findOne({ _id: this.id });
+    this.title = postDocument.title;
+    this.content = postDocument.content;
   }
 
   async save() {
