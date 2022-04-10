ae830d51df3bfa3a668e02b15104fd1bbc9baaa9
fabogit <fabo.dev.email@gmail.com>
2022-04-10 23:12:04 +0200

    day 76
    adding Update & Delete  to the model

--- 31_good-code-refactoring-mvc/models/post.js
+++ 31_good-code-refactoring-mvc/models/post.js
@@ -1,17 +1,48 @@
 const db = require("../data/database");
+const mongodb = require("mongodb");
+const ObjectId = mongodb.ObjectId;
+// const { ObjectId } = require("mongodb");
 
 class Post {
   constructor(title, content, id) {
     this.title = title;
     this.content = content;
-    this.id = id; // may be undefined
+    // may be undefined
+    if (id) {
+      this.id = new ObjectId(id);
+    }
   }
 
   async save() {
-    const result = await db.getDb().collection("posts").insertOne({
-      title: this.title,
-      content: this.content,
-    });
+    let result;
+    // update POST /posts/:id/edit
+    if (this.id) {
+      result = await db
+        .getDb()
+        .collection("posts")
+        .updateOne(
+          { _id: this.id },
+          { $set: { title: this.title, content: this.content } }
+        );
+    } else {
+      // save POST /posts
+      result = await db.getDb().collection("posts").insertOne({
+        title: this.title,
+        content: this.content,
+      });
+    }
+    return result;
+  }
+
+  async delete() {
+    if (!this.id) {
+      // throw new error || return
+      return;
+    }
+    const result = await db
+      .getDb()
+      .collection("posts")
+      .deleteOne({ _id: this.id });
     return result;
   }
 }
