85a3f9efcafe4d1ea33913a8956c81d5587558f9
fabogit <fabo.dev.email@gmail.com>
2022-04-15 11:41:52 +0200

    day 79
    EJS views

--- 32_online-shop/app.js
+++ 32_online-shop/app.js
@@ -1,8 +1,14 @@
+const path = require('path');
+
 const express = require('express');
 
 const authRoutes = require('./routes/auth.routes');
 
 const app = express();
+
+// EJS & views folder
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(authRoutes);
 
--- 32_online-shop/controllers/auth.controller.js
+++ 32_online-shop/controllers/auth.controller.js
@@ -6,7 +6,6 @@ function getLogin(req, res) {
   // ...
 }
 
-
 module.exports = {
   getSignup: getSignup,
   getLogin: getLogin
