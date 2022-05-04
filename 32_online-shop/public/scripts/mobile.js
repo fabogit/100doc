0a7784e2a953feec789ea6bd19ead1fc8cf7bc90
fabogit <fabo.dev.email@gmail.com>
2022-05-04 23:30:33 +0200

    day 84
    frontend JavaScript for toggling the mobile menu

---
+++ 32_online-shop/public/scripts/mobile.js
@@ -0,0 +1,10 @@
+const mobileMenuBtnElement = document.getElementById('mobile-menu-btn');
+const mobileMenuElement = document.getElementById('mobile-menu');
+
+function toggleMobileMenu() {
+  mobileMenuElement.classList.toggle('open');
+}
+
+// event listener, when button is clicked it toggle the menu to show/hide,
+// adding/removing class open to mobile-menu
+mobileMenuBtnElement.addEventListener('click', toggleMobileMenu);
