5b22482aa1f5e7892b591534b61c4be29c79fd1e
fabogit <fabo.dev.email@gmail.com>
2022-05-07 23:02:20 +0200

    day 85
    more data: adding a product model & storing products in the db

--- 32_online-shop/controllers/admin.controller.js
+++ 32_online-shop/controllers/admin.controller.js
@@ -1,3 +1,5 @@
+const Product = require('../models/product.model');
+
 function getProducts(req, res) {
   res.render('admin/products/all-products');
 }
@@ -6,9 +8,20 @@ function getNewProduct(req, res) {
   res.render('admin/products/new-product');
 }
 
-function createNewProduct(req, res) {
+async function createNewProduct(req, res, next) {
   // console.log(req.body);
   // console.log(req.file);
+  const product = new Product({
+    ...req.body,
+    image: req.file.filename
+  });
+  try {
+    await product.save();
+  } catch (error) {
+    next(error);
+    return;
+  }
+
   res.redirect('/admin/products');
 }
 
