5b81eb39857dd15e62a649d4f5cf89c127837e67
fabogit <fabo.dev.email@gmail.com>
2022-05-24 01:08:03 +0200

    Merge branch 'main' into fix

--- 32_online-shop/routes/admin.routes.js
+++ 32_online-shop/routes/admin.routes.js
@@ -9,9 +9,6 @@ router.get('/products', adminController.getProducts);
 router.post('/products', imageUploadMiddleware, adminController.createNewProduct);
 router.get('/products/new', adminController.getNewProduct);
 router.get('/products/:id', adminController.getUpdateProduct);
-router.post('/products/:id', adminController.updateProduct);
-
-router.get('/products/:id', adminController.getUpdateProduct);
 router.post('/products/:id', imageUploadMiddleware, adminController.updateProduct);
 
 module.exports = router;
