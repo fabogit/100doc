82a714ce354e94c8978bd47da0d157b0564539a2
fabogit <fabo.dev.email@gmail.com>
2022-06-17 01:17:55 +0200

    day 99
    updating data

--- 36_frontend-js-frameworks-vue/scripts/app.js
+++ 36_frontend-js-frameworks-vue/scripts/app.js
@@ -2,19 +2,40 @@ const TodosApp = {
 	data() {
 		return {
 			todos: [],
-			enteredTodoText: ''
+			enteredTodoText: '',
+			editiedTodoId: null
 		};
 	},
 	methods: {
 		saveTodo(event) {
 			event.preventDefault();
-			const newTodo = {
-				text: this.enteredTodoText,
-				id: new Date().toISOString()
-			};
-			this.todos.push(newTodo);
+			// create if new/update if exist
+			if (this.editiedTodoId) {
+				const todoId = this.editiedTodoId;
+				const todoIndex = this.todos.findIndex((todoItem) => todoItem.id === todoId);
+				// override item
+				const updatedTodoItem = {
+					id: this.todos[todoIndex],
+					text: this.enteredTodoText
+				};
+				this.todos[todoIndex] = updatedTodoItem;
+				// reset id
+				this.editiedTodoId = null;
+			} else {
+				const newTodo = {
+					text: this.enteredTodoText,
+					id: new Date().toISOString()
+				};
+				this.todos.push(newTodo);
+			}
+
 			// reset to empty
 			this.enteredTodoText = null;
+		},
+		startEditTodo(todoId) {
+			this.editiedTodoId = todoId;
+			const todo = this.todos.find((todoItem) => todoItem.id === todoId);
+			this.enteredTodoText = todo.text;
 		}
 	}
 };
